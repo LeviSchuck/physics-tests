b19fed2c63af0d6292dd72aeee6cda4fc87833dc
Levi Schuck <kloplop321@gmail.com>
2013-03-18 16:18:30 -0600

    Making the spacing nicer.

--- src/main.cpp
+++ src/main.cpp
@@ -10,23 +10,13 @@
 // function provides some Mac OS X specific source code to load files from the resources of the application bundle.
 char * resPath(char **argv, char* name, char* type ) 
 {
-   static char resource[1024];
-   
-   CFURLRef cfBundleURL = CFBundleCopyResourceURL( CFBundleGetMainBundle(),    
-									   CFStringCreateWithCString(kCFAllocatorDefault,
-														  name, 
-														  kCFStringEncodingISOLatin1), 
-									   CFStringCreateWithCString(kCFAllocatorDefault, 
-														  type, 
-														  kCFStringEncodingISOLatin1), 
-									   NULL );
-   
-   
-   CFStringGetCString( CFURLCopyPath(cfBundleURL),
-				  resource,
-				  1023,
-				  kCFStringEncodingISOLatin1);
-   
+	static char resource[1024];
+	CFURLRef cfBundleURL = CFBundleCopyResourceURL( CFBundleGetMainBundle(),    
+	CFStringCreateWithCString(kCFAllocatorDefault,name, kCFStringEncodingISOLatin1), 
+	CFStringCreateWithCString(kCFAllocatorDefault,type,kCFStringEncodingISOLatin1), 
+	NULL);
+	CFStringGetCString( CFURLCopyPath(cfBundleURL),
+		resource,1023,kCFStringEncodingISOLatin1);
    return resource;
 }
 #pragma comment(lib, "libMacOSX.a")
